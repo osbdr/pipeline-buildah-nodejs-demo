3af5de2ed97efd8e6b7891661a5009e30978fd47
Jonas Michaelis <jonas.michaelis@outlook.com>
2020-05-29 14:31:27 +0200

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+
+var express = require('express');
+var app = express();
+
+module.exports = app.listen(3000, function () {
+    console.log('Ready for connections (Port: 3000)');
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello Docker!');
+});
+
+
+
